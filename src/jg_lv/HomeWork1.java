5231bac2d45a8d2fa55fd2780e3cb1808766389a
eciguze <erikaciguze@gmail.com>
2019-10-22 22:47:15 +0300

    Initial commit

---
+++ src/jg_lv/HomeWork1.java
@@ -0,0 +1,25 @@
+package jg_lv;
+
+import java.util.Random;
+
+public class HomeWork1 {
+        public static void main(String[] args) {
+            Random randomGenerator = new Random();
+            double firstUnit = randomGenerator.nextInt(75);
+            double secondUnit = randomGenerator.nextInt(60);
+
+            double sum = firstUnit + secondUnit;
+            double subtract = firstUnit - secondUnit;
+            double multiply = firstUnit + secondUnit;
+            double div = firstUnit / secondUnit;
+
+            System.out.println("First Unit:  " + firstUnit);
+            System.out.println("Second Unit:  " + secondUnit);
+
+            System.out.println("Sum Result:  " + sum);
+            System.out.println("Subtract Result:  " + subtract);
+            System.out.println("Multiply Result:  " + multiply);
+            System.out.println("Divide Result:  " + div);
+        }
+    }
+
